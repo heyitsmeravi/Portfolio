9bc6043a9ed52d15cf958a20cc1bcf0005fc7342
heyitsmeravi <ravijnv2428@gmail.com>
2025-08-03 17:26:42 +0530

    Melodix added

---
+++ Projects/Melodix/script.js
@@ -0,0 +1,118 @@
+console.log("Welcome to Melodix");
+let masterPlay=document.querySelector(".masterPlay");
+let myProgressBar=document.querySelector(".progress-bar");
+let prvBtn=document.querySelector(".prvBtn");
+let nxtBtn=document.querySelector(".nxtBtn");
+let songIndex=0;
+let songs=[
+    {
+songName:"Let me love you", songPath:"asset/1.mp3", songCover:"asset/1.png"
+},{
+    songName:"Attention", songPath:"asset/2.mp3", songCover:"asset/2.png"
+},
+{
+    songName:"Tum hi ho", songPath:"asset/3.mp3", songCover:"asset/3.png"
+},
+{
+    songName:"Chandaniya", songPath:"asset/4.mp3", songCover:"asset/4.jpg"
+}, 
+{
+    songName:"Kesariya", songPath:"asset/5.mp3", songCover:"asset/5.jpeg"
+},
+{
+    songName:"Midnight changes", songPath:"asset/6.mp3",songCover:"asset/6.jpeg"
+}
+];
+let audioElement= new Audio('asset/1.mp3');
+// audioElement.play();
+masterPlay.addEventListener("click", ()=>{
+if (audioElement.paused || audioElement.currentTime<=0){
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+}
+else{
+    songEnd.innerText=formatTime(audioElement.duration);
+    audioElement.pause();
+    masterPlay.classList.remove('fa-pause-circle');
+    masterPlay.classList.add('fa-play-circle');
+
+}
+});
+function formatTime(seconds) {
+    let mins = Math.floor(seconds / 60);
+    let secs = Math.floor(seconds % 60);
+    if (secs < 10) secs = "0" + secs;
+    return `${mins}:${secs}`;
+}
+
+let songStart=document.querySelector("#songStart");
+let songEnd=document.querySelector("#songEnd");
+audioElement.addEventListener('timeupdate',()=>{
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+    songStart.innerText=formatTime(audioElement.currentTime);
+
+    
+});
+myProgressBar.addEventListener("change",()=>{
+    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+});
+let cardPlay = document.querySelectorAll(".card");
+let currPlay = document.querySelector(".currSongName");
+let currSongImg= document.querySelector(".currSongImg");
+
+cardPlay.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    // Load the selected song
+    audioElement.src = songs[index].songPath;
+    audioElement.play();
+
+    // Update play/pause icon
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+
+    // Update UI
+    currPlay.innerText = songs[index].songName;
+    currSongImg.src=songs[index].songCover;
+
+    // Update progress time at end
+    audioElement.addEventListener("loadedmetadata", () => {
+    songEnd.innerText = formatTime(audioElement.duration);
+    });
+  });
+});
+prvBtn.addEventListener("click",()=>{
+    if (songIndex){
+        songIndex--;
+        audioElement.src=songs[songIndex].songPath;
+        audioElement.play();
+        currPlay.innerText = songs[songIndex].songName;
+    currSongImg.src=songs[songIndex].songCover;
+    }
+});
+nxtBtn.addEventListener("click",()=>{
+        songIndex++;
+        audioElement.src=songs[songIndex].songPath;
+        audioElement.play();
+        currPlay.innerText = songs[songIndex].songName;
+        currSongImg.src=songs[songIndex].songCover;
+}); 
+
+//volume functionalities
+let volumeIcon=document.querySelector(".volume span");
+let volumeBar=document.querySelector(".volumebar");
+volumeBar.addEventListener("input",(e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    audioElement.volume=volumeBar.value/100;
+    if (volumeBar.value==0){
+        volumeIcon.innerText="volume_mute";
+    }
+    else if (volumeBar.value>0 && volumeBar.value<=40){
+        volumeIcon.innerText="volume_down";
+    }
+    else if(volumeBar.value>40 && volumeBar.value<=100){
+        volumeIcon.innerText="volume_up";   
+    }
+});
